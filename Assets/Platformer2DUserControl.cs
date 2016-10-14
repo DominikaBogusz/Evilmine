d43947491067cb32aa1ae25e692d5230b9bf7e1a
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-10-14 10:40:44 +0200

    Edited UserControl & Character2D scripts to more readible form, added dwarf sprites & animations

---
+++ Assets/Platformer2DUserControl.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+            if (!jump)
+            {
+                jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            character.Move(h, jump);
+            jump = false;
+        }
+    }
+}
