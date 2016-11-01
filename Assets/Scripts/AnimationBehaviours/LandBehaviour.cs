613965e9bdcedba2b695b6930c42a60cf421d0f5
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-11-01 09:57:58 +0100

    Cleaning in project folder

---
+++ Assets/Scripts/AnimationBehaviours/LandBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LandBehaviour : StateMachineBehaviour {
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Player.Instance.OnGround)
+        {
+            animator.SetBool("land", false);
+        }
+    }
+}
