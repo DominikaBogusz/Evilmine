71864d2d61a1c0d9ce4b3a8c074795040ea39d82
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-11-05 09:22:39 +0100

    First attempt to make camera follow/skip

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float xMax;
+    [SerializeField]
+    private float yMin;
+    [SerializeField]
+    private float yMax;
+
+    void Start()
+    {
+        GetComponent<Camera>().orthographicSize = 5.5f;
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+	}
+}
