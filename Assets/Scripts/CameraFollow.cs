5bbaad978496271db3961b3bbfae3e80ea8b306e
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-11-05 12:18:10 +0100

    Simple camera follow after all (for now at least)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,22 +5,8 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField]
     private Transform target;
 
-    [SerializeField]
-    private float xMin;
-    [SerializeField]
-    private float xMax;
-    [SerializeField]
-    private float yMin;
-    [SerializeField]
-    private float yMax;
-
-    void Start()
-    {
-        GetComponent<Camera>().orthographicSize = 5.5f;
-    }
-
     void LateUpdate ()
     {
-        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
 	}
 }
