e772a5a775c994bc21a47905cd42fbfb1a5e94f5
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-11-07 23:17:23 +0100

    Still struggling with camera

--- Assets/CameraAdjust.cs
+++ Assets/CameraAdjust.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class CameraAdjust : MonoBehaviour {
 
     [SerializeField] private Transform target;
 
-    [SerializeField] private BoxCollider2D LevelBounds;
+    //public List<BoxCollider2D> ActiveBoundaries { get; set; }
+    [SerializeField] BoxCollider2D firstBoundaries;
+    public List<BoxCollider2D> ActiveBoundaries;
 
-    private Vector3 min, max;
+    float left, right, up, down;
+
+    [SerializeField] BoxCollider2D leftBar, rightBar, upBar, downBar;
+
+    float cameraHalfWidth;
 
     void Start()
     {
-        min = LevelBounds.bounds.min;
-        max = LevelBounds.bounds.max;
+        //ActiveBoundaries.Add(firstBoundaries);
+        
+        /*
+        left = ActiveBoundaries[0].bounds.min.x;
+        right = ActiveBoundaries[0].bounds.max.x;
+        down = ActiveBoundaries[0].bounds.min.y;
+        up = ActiveBoundaries[0].bounds.max.y;
+        */
+
+        cameraHalfWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+    }
+
+    void Awake()
+    {
+        float height = 2 * GetComponent<Camera>().orthographicSize;
+        float width = height * GetComponent<Camera>().aspect;
+
+        leftBar.offset = new Vector2(-width / 2, 0f);
+        rightBar.offset = new Vector2(width / 2, 0f);
+        upBar.offset = new Vector2(0f, height / 2);
+        downBar.offset = new Vector2(0f, -height / 2);
     }
 
 
@@ -28,13 +54,45 @@ public class CameraAdjust : MonoBehaviour {
 
     private void AdjustToBounds()
     {
-        float cameraHalfWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+        if (ActiveBoundaries.Count == 1)
+        {
+            left = ActiveBoundaries[0].bounds.min.x;
+            right = ActiveBoundaries[0].bounds.max.x;
+            down = ActiveBoundaries[0].bounds.min.y;
+            up = ActiveBoundaries[0].bounds.max.y;
+        }
+        else
+        {
+            foreach (BoxCollider2D box in ActiveBoundaries)
+            {
+                float aLeft = box.bounds.min.x;
+                if (aLeft < left)
+                {
+                    left = aLeft;
+                }
+                float aRight = box.bounds.max.x;
+                if (aRight > right)
+                {
+                    right = aRight;
+                }
+                float aDown = box.bounds.min.y;
+                if (aDown < down)
+                {
+                    down = aDown;
+                }
+                float aUp = box.bounds.max.y;
+                if (aUp > up)
+                {
+                    up = aUp;
+                }
+            }
+        }
 
         // lock the camera to the right or left bound if we are touching it
-        float x = Mathf.Clamp(transform.position.x, min.x + cameraHalfWidth, max.x - cameraHalfWidth);
+        float x = Mathf.Clamp(transform.position.x, left + cameraHalfWidth, right - cameraHalfWidth);
 
         // lock the camera to the top or bottom bound if we are touching it
-        float y = Mathf.Clamp(transform.position.y, min.y + GetComponent<Camera>().orthographicSize, max.y - GetComponent<Camera>().orthographicSize);
+        float y = Mathf.Clamp(transform.position.y, down + GetComponent<Camera>().orthographicSize, up - GetComponent<Camera>().orthographicSize);
 
         transform.position = new Vector3(x, y, transform.position.z);
     }
---
+++ Assets/CameraBoundary.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraBoundary : MonoBehaviour {
+
+    [SerializeField] private CameraAdjust cam;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "RightBar")
+        {
+            cam.ActiveBoundaries.Add(GetComponent<BoxCollider2D>());
+            Debug.Log("added");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "LeftBar")
+        {
+            cam.ActiveBoundaries.Remove(GetComponent<BoxCollider2D>());
+            Debug.Log("removed");
+        }
+    }
+}
