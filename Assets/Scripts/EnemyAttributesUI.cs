e7358f61971b86fadcb49050d8e54d81b9b2436f
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-11-18 18:03:04 +0100

    Added canvas above charactes with their attributes

---
+++ Assets/Scripts/EnemyAttributesUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyAttributesUI : MonoBehaviour {
+
+    private Enemy enemy;
+
+	[SerializeField] private Text healthValue;
+    [SerializeField] private Text damageValue;
+    [SerializeField] private Text attackSpeedValue;
+    [SerializeField] private Text attackIntervalValue;
+
+    void Start()
+    {
+        enemy = transform.parent.GetComponent<Enemy>();
+    }
+
+	void Update ()
+    {
+        if(transform.parent.localScale.x == -1)
+        {
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector2(1f, 1f);
+        }
+
+        healthValue.text = enemy.attributes.Health.ToString();
+        damageValue.text = enemy.attributes.Damage.ToString();
+        attackSpeedValue.text = enemy.attributes.AttackSpeed.ToString();
+        attackIntervalValue.text = enemy.attributes.AttackInterval.ToString();
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (GetComponent<Canvas>().enabled)
+            {
+                GetComponent<Canvas>().enabled = false;
+            }
+            else
+            {
+                GetComponent<Canvas>().enabled = true;
+            }
+        }
+    }
+}
